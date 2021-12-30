54d114906f43fc67dc52b57ddf78e9ea365a4aeb
MatthewCharlton <33848730+MatthewCharlton@users.noreply.github.com>
2021-12-30 14:26:59 +1100

    Add files via upload

---
+++ assets/index.f2280d02.js
@@ -0,0 +1,1 @@
+import{t as r,p as s,q as a}from"./vendor.43e5c33f.js";const c=r('<img class="w-[1.2rem] m-1">'),n=t=>(()=>{const e=c.cloneNode(!0);return s(()=>a(e,"src",t.src)),e})();export{n as default};
