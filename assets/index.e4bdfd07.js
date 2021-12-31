e50615dbbaef4c3c1babdf3f7f7a5aaa147b1ab4
MatthewCharlton <33848730+MatthewCharlton@users.noreply.github.com>
2021-12-31 12:13:47 +1100

    Delete assets directory

--- assets/index.e4bdfd07.js
+++
@@ -1,1 +0,0 @@
-import{t as r,p as s,q as a}from"./vendor.bb69e759.js";const c=r('<img class="w-[1.2rem] m-1">'),n=t=>(()=>{const e=c.cloneNode(!0);return s(()=>a(e,"src",t.src)),e})();export{n as default};
