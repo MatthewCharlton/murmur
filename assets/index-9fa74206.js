c94887c71bc794a0fd32f5439805849b1ecf7e53
MatthewCharlton <33848730+MatthewCharlton@users.noreply.github.com>
2022-12-29 15:50:42 +1100

    Delete assets directory

--- assets/index-9fa74206.js
+++
@@ -1,1 +0,0 @@
-import{f as r,d as s,t as a}from"./index-cabe918c.js";import"./browser-ponyfill-5ce9a37a.js";const c=a('<img class="w-[1.2rem] m-1">'),n=e=>(()=>{const t=c.cloneNode(!0);return r(()=>s(t,"src",e.src)),t})();export{n as default};
